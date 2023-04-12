fe51475eb6b2486668d273aa55e50bca936b65d3
Johann Hartmann <johannhartmann@web.de>
2023-04-12 10:46:48 +0200

    updated artifacts

--- artifacts/script_init.py
+++ artifacts/script_init.py
@@ -1,62 +1,43 @@
-# ---------------------------------------------------------------------------- #
-#                FA2 implementation using SmartPy Exmperimental                #
-#                  https://smartpy.io/docs/guides/FA/FA2_lib/                  #
-# ---------------------------------------------------------------------------- #
-
 import smartpy as sp
-FA2 = sp.io.import_script_from_url("https://smartpy.io/templates/fa2_lib.py")
+
+TTransferObject = sp.TRecord(consumerId=sp.TString, producerId=sp.TString,
+                             assetId=sp.TString, timestamp=sp.TTimestamp, contractRef=sp.TAddress)
 
 
-class ExampleFa2Nft(FA2.Admin, FA2.Fa2Nft):
-    def __init__(self, admin, metadata, metadata_base):
-        # no token transfer allowed
-        FA2.Fa2Nft.__init__(
-            self, metadata, metadata_base=metadata_base, policy=FA2.NoTransfer())
-        FA2.Admin.__init__(self, admin)
-        # modify initial contract storage for testing
-        self.update_initial_storage(
-            author="Johann Hartmann",
+class TransferStore(sp.Contract):
+    def __init__(self):
+        # init map for storing data transfer data
+        self.init(
+            admin=sp.set([sp.address("tz1RTt21hfc9rndKcQTS1CeF5rzr8bJ5nhV5")]), dataTransferMap=sp.map(tkey=sp.TNat, tvalue=TTransferObject)
         )
-    # customize mint entry point
 
-    @sp.entry_point
-    def mint(self, batch):
-        sp.set_type(
-            batch,
-            sp.TList(
-                sp.TRecord(
-                    to_=sp.TAddress,
-                    metadata=sp.TMap(sp.TString, sp.TBytes),
-                ).layout(("to_", "metadata"))
-            ),
-        )
-        # anyone can mint in this case
-        with sp.for_("action", batch) as action:
-            token_id = sp.compute(self.data.last_token_id)
-            metadata = sp.record(token_id=token_id, token_info=action.metadata)
-            self.data.token_metadata[token_id] = metadata
-            self.data.ledger[token_id] = action.to_
-            self.data.last_token_id += 1
+    @ sp.entry_point(name="postDataTransfer")
+    def postDataTransfer(self, transactionId, consumerId, producerId, assetId, contractRef):
+        # check if transactionId is already used
+        sp.verify(~self.data.dataTransferMap.contains(transactionId),
+                  message="TransactionId already used.")
+        # store data transfer data
+        self.data.dataTransferMap[transactionId] = sp.record(
+            consumerId=consumerId, producerId=producerId, assetId=assetId, timestamp=sp.now, contractRef=contractRef)
+
+    @ sp.entry_point(name="getDataTransfer")
+    def getDataTransfer(self, transactionId):
+        # check if transactionId exists
+        sp.verify(self.data.dataTransferMap.contains(transactionId))
+
+    @ sp.entry_point(name="deleteDataTransfer")
+    def deleteDataTransfer(self, transactionId):
+        # check if sender is admin
+        sp.verify(self.data.admin.contains(sp.source),
+                  message="No permission to write to smart contract.")
+        # check if transactionId exists
+        sp.verify(self.data.dataTransferMap.contains(transactionId))
+        # delete data transfer data
+        del self.data.dataTransferMap[transactionId]
 
 
+# add compilation target
 sp.add_compilation_target(
-    # metadata json file should be generated under the name "metadata_base" which will contain the offchain views as well
-    "contractContract",
-    ExampleFa2Nft(admin=sp.address("tz1Na21NimuuPXcQdHUk2en2XWYe9McyDDgZ"), metadata=sp.utils.metadata_of_url(
-        "https://ipfs.io/ipfs/QmTvQA5UMXK6bCYjT8UUApJuMKBJgynRZ5745AQewNkvcC"), metadata_base={
-            "name": "gaia.tub.assets",
-            "version": "1.0.0",
-            "description": "NFT contract for ipfs asset management",
-            "interfaces": ["TZIP-012", "TZIP-016"],
-            "authors": ["Technische Universität Berlin", "Information Systems Engineering", "Johann Hartmann"],
-            "homepage": "https://www.ise.tu-berlin.de/menue/projekte/",
-            "source": {
-                "tools": ["SmartPy"],
-                "location": "https://gitlab.com/SmartPy/smartpy/-/raw/master/python/templates/FA2.py"
-            },
-            "permissions": {
-                "receiver": "owner-no-hook",
-                "sender": "owner-no-hook"
-            }
-    })
+    "transferContract",
+    TransferStore()
 )

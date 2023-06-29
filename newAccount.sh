469dbe5b5843347c1b214b694ae59c787bde6b07
Johann Hartmann <johannhartmann@web.de>
2023-06-29 12:19:54 +0200

    fix: redundant account creation

--- newAccount.sh
+++ newAccount.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Set ghostnet as the default network
-octez-client --endpoint https://rpc.ghostnet.teztnets.xyz/ config update
+octez-client --endpoint https://rpc.ghostnet.teztnets.xyz/ config update 2>/dev/null
 
 # Generate a new account using the octez-client
 account_name="edc-account"
 
-octez-client gen keys $account_name
+octez-client gen keys $account_name --force 2>/dev/null
 
 echo "New account generated"
 octez-client show address $account_name

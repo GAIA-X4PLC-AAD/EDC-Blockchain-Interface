e93b9167f2901df12a18749df4294db606a1dc73
uZhW8Rgl <sonak53@googlemail.com>
2023-11-30 14:16:43 +0100

    changed the octez-client variable

--- newAccount.sh
+++ newAccount.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Set ghostnet as the default network
-octez-client --endpoint https://rpc.ghostnet.teztnets.xyz/ config update 2>/dev/null
+docker exec octez-node-alpha octez-client --endpoint https://rpc.ghostnet.teztnets.xyz/ config update 2>/dev/null
 
 # Generate a new account using the octez-client
 account_name="edc-account"
 
-octez-client gen keys $account_name --force 2>/dev/null
+docker exec octez-node-alpha octez-client gen keys $account_name --force 2>/dev/null
+
 
 echo "New account generated"
-octez-client show address $account_name
+docker exec octez-node-alpha octez-client show address $account_name
 
 # Link to a website
 echo "Visit https://faucet.ghostnet.teztnets.xyz to fund your account"
